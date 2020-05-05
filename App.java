2c08a572d159333defa93459ad37c7d59da9afe3
BrunoBrandao <bruno8117@gmail.com>
2020-05-05 20:04:15 -0300

    Introducao da classe listaclientes e atualizaçoes da classe app

--- App.java
+++ App.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class App {
    public static void main(String args[]){
       
+      ArrayList clientes = new ArrayList<Cliente>(100);
+
       Scanner in = new Scanner(System.in);
       
       System.out.println("Digite 1 para o menu de funcionarios: ");
@@ -13,13 +16,33 @@ public class App {
          case 1:
             System.out.println("Bem vindo ao menu de funcionários; ");
             System.out.println("Digite 1 para cadastrar novo cliente; ");
-            System.out.println("Digite 2 para consultar a disponibilidade de atuomóvel por categoria; ");
+            System.out.println("Digite 2 para consultar a disponibilidade de automóvel por categoria; ");
             System.out.println("Digite 3 para consultar o valor de uma locação; ");
             System.out.println("Digite 4 para realizar uma locação; ");
             System.out.println("Digite 5 para finalizar uma locação; ");
             int a = in.nextInt();
             switch(a){
-               case 1: break;
+               case 1: 
+                  System.out.println("Digite o nome do(a) cliente: ");
+                  String nome = in.nextLine();
+                  System.out.println("Digite o telefone do(a) cliente: ");
+                  String telefone = in.nextLine();
+                  System.out.println("Digite 1 para cliente pessoa-física; "); 
+                  System.out.println("Digite 2 para cliente pessoa-júridica; ");
+                  int pessoa = in.nextInt();
+
+                  switch(pessoa){
+                     case 1: 
+                        System.out.println("Insira o CPF do(a) cliente: ");
+                        String cpf = in.nextLine();   
+                        PessoaFisica umCliente = new PessoaFisica(nome, telefone, cpf);
+                        clientes.insere(umCliente);
+                     case 2: 
+                        System.out.println("Insira o CNPJ da empresa: ");
+                        String cnpj = in.nextLine();
+                        PessoaJuridica umClienteJ = new PessoaJuridica(nome, telefone, cnpj);
+                        clientes.insere(umClienteJ);
+               }
                case 2: break;
                case 3: break;
                case 4: break;
--- ListaClientes.java
+++ ListaClientes.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class ListaClientes {
+    private ArrayList<Cliente> clientes;
+    private int total = 0;
+    private int qtdMax;
+
+    public ListaClientes(int max) {
+        this.qtdMax = max;
+        clientes = new ArrayList<Cliente>(qtdMax);
+    }
+
+    public void insere(Cliente novoCliente) {
+        clientes.add(novoCliente);
+        total = total + 1;
+    }
+
+    public boolean remove(String nome) {
+        for (int i = 0; i < clientes.size(); i++) {
+            Cliente umCliente = clientes.get(i);
+            String umNome = clientes.get(i).getNome();
+            if (nome.equals(umNome)){
+                clientes.remove(umCliente);
+                total = total - 1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void mostraTotal(){
+        System.out.print(total);
+    }
+
+    public void mostra() {
+        if (total < 1) System.out.print("Não há automóveis cadastrados");
+        if (total > 0) {
+            String str = " ";
+            str = "Lista de Automóveis:" + "\n";
+            for (int i = 0; i < clientes.size(); i++) {
+                str = str + clientes.get(i).toString() + "\n" + "//////////////////////";
+            }
+            System.out.println(str);
+        }
+    }
+
+
 }
