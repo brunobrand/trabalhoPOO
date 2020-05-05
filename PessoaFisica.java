e7afc8bb4738ead3f527eb8b5aadaccf18323a7a
Felipe Silveira <zfelipesilveira@gmail.com>
2020-05-05 01:37:10 -0300

    Correção PessoaFisica

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,31 +1,19 @@
-<<<<<<< HEAD
 public class PessoaFisica extends Cliente {
-    private String cpf;
+    private String cpf = "";
 
-    public PessoaFisica(String nome, String telefone, String cpf){
+    public PessoaFisica(String nome, String telefone, String cpf) {
         super(nome, telefone);
         this.cpf = cpf;
     }
 
-    public String getCpf(){
+    public String getCpf() {
         return cpf;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String str = super.toString();
         str = str + "CPF: " + cpf;
         return str;
-=======
-
-public class PessoaFisica extends Cliente{
-   
-    private String cpf = "";
-    
-    public PessoaFisica(String nome, String telefone, String cpf){
-       this.nome = nome;
-       this.telefone = telefone;
-       this.cpf = cpf;
->>>>>>> 20a6a6114d518f0e7bca0dd96ebd31dea0785862
     }
 }
